1b8474d17412a380a5284991b26998f36851a4cb
arameshraju <alluri.ramesh@gmail.com>
2016-12-08 21:49:14 +0530

    Added ui-router configuration

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -1,2 +1,48 @@
-var app=angular.module('ngAdminCart',['ui.bootstrap']);
+(function () {
+'use strict';
 
+var app=angular.module('ngAdminCart',['ui.bootstrap','ui.router']);
+
+app.config(RouteConfig);
+
+RouteConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+function RouteConfig($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+        .state('dash',{
+        url:'/',
+        templateUrl:'views/dashboard.html'
+    })
+    .state('pending',{
+        url:'/pending',
+        templateUrl:'views/pending.html'
+    })
+    .state('stockcreate',{
+        url:'/stockcreate',
+        templateUrl:'views/stock_create.html'
+    })
+    .state('stockentry',{
+        url:'/stockentry',
+        templateUrl:'views/stock_entry.html'
+        
+    })
+    .state('itemoffer',{
+        url:'/itemoffer',
+        templateUrl:'views/item_offer.html'
+    })
+    .state('globaloffer',{
+        url:'/globaloffer',
+        templateUrl:'views/global_offers.html'
+    })
+    .state('slide',{
+        url:'/slide',
+        templateUrl:'views/slides.html'
+    })
+    .state('login',{
+        url:'/login',
+        templateUrl:'views/login.html'
+    });
+};
+
+
+ })();
